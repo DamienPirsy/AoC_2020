5fcf105a003e11f511967416ba817f30cb4b045f
Matteo Vignoli <m.vignoli@lunalabs.it>
2020-12-17 09:58:22 +0100

    Day 10 part 2

--- 10/day10.php
+++ 10/day10.php
@@ -33,7 +33,49 @@ function solve_one($input) : string
 }
 
 
-function solve_two($input) : string
+/**
+ * Thanks to @https://github.com/pixelomer/AdventOfCode/blob/main/2020/Day%2010%20-%20Adapter%20Array/solution.js
+ * for the solution, and the Reddit Solution thread https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/
+ * for all the illuminating insights to dynamic programming
+ *
+ * @param string $input
+ * @return string
+ */
+function solve_two(string $input) : string
 {
-    return "";
+    $numbers = xplode_input($input, true);
+    sort($numbers);
+    $highest = $numbers[count($numbers)-1];
+    array_push($numbers, $highest+3); // append the device adapter to the list
+    
+    $cache = []; // damn memoization approach!
+    return sprintf("Result: %d\n", find_combinations(0, $numbers, $highest, $cache));
+}
+
+
+/**
+ *
+ * @param integer $currentEnd
+ * @param array $adapters
+ * @param integer $highest
+ * @param array $cache
+ * @return integer
+ */
+function find_combinations(int $currentEnd, array $adapters, int $highest, array &$cache) : int{
+    if ($currentEnd == $highest) {
+        return 1;
+    }
+    $count = 0;
+    for ($i=1; $i<=3; $i++) {
+        if (in_array($currentEnd+$i, $adapters)) {
+            $remaining = array_filter($adapters, function($val) use ($currentEnd, $i) {
+                return $val > $currentEnd+$i;
+            });            
+            if (!isset($cache[$currentEnd+$i])) {
+                $cache[$currentEnd+$i] = find_combinations($currentEnd+$i, $remaining, $highest, $cache);
+            }
+            $count += $cache[$currentEnd+$i];
+        }
+    }
+    return $count;
 }
