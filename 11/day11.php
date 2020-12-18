af18fb4a9cff8683ae610366327eadb304d2b698
Matteo Vignoli <m.vignoli@lunalabs.it>
2020-12-18 17:57:09 +0100

    Day 11

--- 11/day11.php
+++ 11/day11.php
@@ -1,5 +1,6 @@
 <?php
 
+use phpDocumentor\Reflection\Types\Callable_;
 
 function solve_one(string $input) : string
 {
@@ -12,8 +13,9 @@ function solve_one(string $input) : string
      */
 
     $seats = array_map('str_split', xplode_input($input));
+    $limit = 4;
     while(true) {
-        [$seats, $hasChanged, $occupied] = walk_matrix($seats);
+        [$seats, $hasChanged, $occupied] = walk_matrix($seats, $limit, 'occupied_neighbors');
         if (!$hasChanged) {
             return sprintf("Occupied seats: %d\n", $occupied);
         }
@@ -25,31 +27,41 @@ function solve_one(string $input) : string
  *
  * @param array $seats
  * @param string $stringRep
+ * @param int $limit
+ * @param Callable $method
  * @return array
  */
-function walk_matrix(array $seats): array 
+function walk_matrix(array $seats, int $limit, Callable $method): array 
 {
     $new = [];
     $hasChanged = false;
     $occupied = 0;
+
+    $h = count($seats);
+    $w = count($seats[0]);    
     foreach ($seats as $y => $row) {        
         foreach ($row as $x => $place) {
-            if ($place == '#') {
-                $occupied++;
-            }
-            $occupiedNeighbors = occupied_neighbors($seats, $x, $y);
-            if ($place == 'L' && $occupiedNeighbors === 0) {
-                $new[$y][$x] = '#';
-                $hasChanged = true;
-                $occupied++;
-            } elseif ($place == '#' && $occupiedNeighbors >= 4) {
-                $new[$y][$x] = 'L';
-                $hasChanged = true;
-                $occupied--;
+            if ($place != '.') {
+                if ($place == '#') {
+                    $occupied++;
+                }
+                $occupiedNeighbors = $method($seats, $x, $y, $h, $w);
+                if ($place == 'L' && $occupiedNeighbors === 0) {
+                    $new[$y][$x] = '#';
+                    $hasChanged = true;
+                    $occupied++;
+                } elseif ($place == '#' && $occupiedNeighbors >= $limit) {
+                    $new[$y][$x] = 'L';
+                    $hasChanged = true;
+                    $occupied--;
+                } else {
+                    $new[$y][$x] = $place;
+                }
             } else {
                 // leave as is
                 $new[$y][$x] = $place;
             }
+
         }
     }
     return [$new, $hasChanged, $occupied];
@@ -59,9 +71,11 @@ function walk_matrix(array $seats): array
  * @param array $seats
  * @param integer $placeX
  * @param integer $placeY
+ * @param integer $w
+ * @param integer $h
  * @return int
  */
-function occupied_neighbors(array $seats, int $placeX, int $placeY) : int 
+function occupied_neighbors(array $seats, int $placeX, int $placeY, int $w, int $h) : int 
 {
     $n = [
         $seats[$placeY-1][$placeX-1] ?? '',
@@ -78,8 +92,143 @@ function occupied_neighbors(array $seats, int $placeX, int $placeY) : int
     return array_count_values($n)['#'] ?? 0;
 }
 
+/**
+ *
+ * @param array $seats
+ * @param integer $placeX
+ * @param integer $placeY
+ * @return integer
+ */
+function visible_neighbors(array $seats, int $placeX, int $placeY, int $w, int $h) : int
+{
+    $occupied = 0;
+
+    $y = $placeY;
+    $x = $placeX;
+    while ($y >= 0 && $x >= 0) {
+        $x = $x-1;
+        $y = $y-1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+    //top (y-i, x) untiil y=0
+    $y = $placeY;
+    $x = $placeX;    
+    while ($y >= 0) {
+        $y = $y-1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+    //topright (y-, x+i) until y=0, x = width
+    $y = $placeY;
+    $x = $placeX;    
+    while ($y >= 0 && $x<=$w) {
+        $y = $y-1;
+        $x = $x+1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+
+    //left (y, x-i) until x = 0
+    $y = $placeY;
+    $x = $placeX;    
+    while ($x >= 0) {
+        $x = $x-1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+
+    //right (y, x+i) untiil x = w
+    $y = $placeY;
+    $x = $placeX;    
+    while ($x <= $w) {
+        $x = $x+1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+
+    // bottomleft (y+i, x-i) until y=h, x=0
+    $y = $placeY;
+    $x = $placeX;    
+    while ($x >= 0 && $y <= $h) {
+        $x = $x-1;
+        $y = $y+1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+
+    // bottom (y+i, x) until y = h
+    $y = $placeY;
+    $x = $placeX;    
+    while ($y <= $h) {
+        $y = $y+1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+    
+    //bottomright (y+1, x+1) until y = h, x = w
+    $y = $placeY;
+    $x = $placeX;    
+    while ($y <= $h && $x <= $w) {
+        $x = $x+1;
+        $y = $y+1;
+        if (isset($seats[$y][$x]) && $seats[$y][$x] != '.') {
+            if ($seats[$y][$x] == '#') {
+                $occupied++;
+            }
+            break;
+        }
+    }
+    return $occupied;
+}
+
 function solve_two(string $input) : string
 {
-    return "";
+    /**
+     * As soon as people start to arrive, you realize your mistake. People don't just care about adjacent seats - 
+     * they care about the first seat they can see in each of those eight directions!
+     * 
+     * Also, people seem to be more tolerant than you expected: it now takes five or more visible occupied seats for 
+     * an occupied seat to become empty (rather than four or more from the previous rules). 
+     * The other rules still apply: empty seats that see no occupied seats become occupied, seats matching no rule don't change, 
+     * and floor never changes.
+     */
+
+     $seats = array_map('str_split', xplode_input($input));
+    $limit = 5;
+    while(true) {
+        [$seats, $hasChanged, $occupied] = walk_matrix($seats, $limit, 'visible_neighbors');
+        if (!$hasChanged) {
+            return sprintf("Occupied seats: %d\n", $occupied);
+        }
+    }
 }
 
